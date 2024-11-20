8d5e257ef5853ad990abffef5c66120b476ff3e0
Filip <filip26@gmail.com>
2024-11-20 15:52:35 +0100

    Fix proof value extraction

--- src/main/java/com/apicatalog/ld/signature/eddsa/EdDSASignature2022.java
+++ src/main/java/com/apicatalog/ld/signature/eddsa/EdDSASignature2022.java
@@ -1,16 +1,24 @@
 package com.apicatalog.ld.signature.eddsa;
 
+import java.net.URI;
+
 import com.apicatalog.controller.key.KeyPair;
+import com.apicatalog.controller.method.VerificationMethod;
 import com.apicatalog.cryptosuite.CryptoSuite;
 import com.apicatalog.cryptosuite.primitive.MessageDigest;
 import com.apicatalog.cryptosuite.primitive.Urdna2015;
+import com.apicatalog.jsonld.loader.DocumentLoader;
 import com.apicatalog.ld.DocumentError;
+import com.apicatalog.ld.DocumentError.ErrorType;
 import com.apicatalog.multibase.Multibase;
 import com.apicatalog.multicodec.MulticodecDecoder;
 import com.apicatalog.multicodec.codec.KeyCodec;
 import com.apicatalog.vc.issuer.Issuer;
+import com.apicatalog.vc.model.VerifiableMaterial;
 import com.apicatalog.vc.proof.ProofValue;
 import com.apicatalog.vc.solid.SolidIssuer;
+import com.apicatalog.vc.solid.SolidProofValue;
+import com.apicatalog.vcdi.DataIntegrityProofDraft;
 import com.apicatalog.vcdi.DataIntegritySuite;
 
 public final class EdDSASignature2022 extends DataIntegritySuite {
@@ -36,57 +44,25 @@ public final class EdDSASignature2022 extends DataIntegritySuite {
     public Issuer createIssuer(KeyPair keyPair) {
         return new SolidIssuer(this, CRYPTO, keyPair, proofValueBase);
     }
-    
-//    public static final MethodAdapter METHOD_ADAPTER = new MultiKeyAdapter(CODECS) {
-//
-//        @Override
-//        protected Multicodec getPublicKeyCodec(String algo, int keyLength) {
-//            return KeyCodec.ED25519_PUBLIC_KEY;
-//        }
-//
-//        @Override
-//        protected Multicodec getPrivateKeyCodec(String algo, int keyLength) {
-//            return KeyCodec.ED25519_PRIVATE_KEY;
-//        }
-//
-//        protected void validate(MultiKey method) throws DocumentError {
-//            if (method.publicKey() != null
-//                    && method.publicKey().length != 32
-//                    && method.publicKey().length != 57
-//                    && method.publicKey().length != 114) {
-//                throw new DocumentError(ErrorType.Invalid, "PublicKeyLength");
-//            }
-//        };
-//    };
 
-//    public EdDSASignature2022() {
-//        super(CRYPTOSUITE_NAME, Multibase.BASE_58_BTC,  METHOD_ADAPTER);
-//    }
+    public DataIntegrityProofDraft createDraft(VerificationMethod verificationMethod, URI purpose) {
+        return new DataIntegrityProofDraft(this, CRYPTO, verificationMethod, purpose);
+    }
 
-//    public DataIntegrityProofDraft createDraft(
-//            VerificationMethod method,
-//            URI purpose) throws DocumentError {
-//        return new DataIntegrityProofDraft(this, CRYPTO, method, purpose);
-//    }
-//
-//    public DataIntegrityProofDraft createDraft(
-//            URI method,
-//            URI purpose) throws DocumentError {
-//        return new DataIntegrityProofDraft(this, CRYPTO, method, purpose);
-//    }
+    public DataIntegrityProofDraft createDraft(URI verificationMethod, URI purpose) {
+        return new DataIntegrityProofDraft(this, CRYPTO, verificationMethod, purpose);
+    }
 
-//    @Override
-//    public Issuer createIssuer(KeyPair keyPair) {
-//        return new SolidIssuer(this, keyPair, proofValueBase);
-//    }
-//
-//    @Override
-//    protected ProofValue getProofValue(byte[] proofValue, DocumentLoader loader) throws DocumentError {
-//        if (proofValue != null && proofValue.length != 64) {
-//            throw new DocumentError(ErrorType.Invalid, "ProofValueLength");
-//        }
-//        return new SolidProofValue(proofValue);
-//    }
+    protected ProofValue getProofValue(VerifiableMaterial data, VerifiableMaterial proof, byte[] proofValue, DocumentLoader loader) throws DocumentError {
+        if (proofValue == null) {
+            return null;
+        }
+
+        if (proofValue.length != 64) {
+            throw new DocumentError(ErrorType.Invalid, "ProofValueLength");
+        }
+        return SolidProofValue.of(CRYPTO, data, proof, proofValue);
+    }
 
     @Override
     protected CryptoSuite getCryptoSuite(String cryptoName, ProofValue proofValue) throws DocumentError {
